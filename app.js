dc7f1ecebc48fcd6a53dbb16683187f30be94824
Kaustav Ganguli <write.to.kaustav@gmail.com>
2018-07-17 15:48:10 -0400

    upload pdf-create page image -feed to tesseract

--- app.js
+++ app.js
@@ -7,13 +7,24 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var formidable = require('formidable');
+var fs = require('fs');
+
+
 var bodyParser     =        require("body-parser");
+const multer = require('multer') // file storing middleware
 
 var upload_pdf = require('./routes/upload_pdf');
 var process_pdf = require('./routes/process_pdf');
 // var take_picture = require('./routes/take_picture');
 // var upload_image = require('./routes/upload_image');
+
+
+
+
+// CREATE UPLOAD FOLDER
+var exists = fs.existsSync(path.join(__dirname,'public/UPLOAD'))
+if(!exists)
+  fs.mkdirSync(path.join(__dirname,'public/UPLOAD'))
 
 
 
@@ -36,23 +47,52 @@ app.use('/users', usersRouter);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+
+
+
+
 app.use('/upload_pdf', upload_pdf);
 
-// app.post('/', function (req, res){
-//   var form = new formidable.IncomingForm();
 
-//   form.parse(req);
 
-//   form.on('fileBegin', function (name, file){
-//       file.path = path.join(__dirname , 'public/UPLOAD/' , file.name);
-//   });
+// MULTER UPLOAD FILE
+// https://medium.com/@Moonstrasse/how-to-make-a-basic-html-form-file-upload-using-multer-in-an-express-node-js-app-16dac2476610
 
-//   form.on('file', function (name, file){
-//       console.log('Uploaded ' + file.name);
-//   });
+const multerConfig = {
+    
+  storage: multer.diskStorage({
+   //Setup where the user's file will go
+   destination: function(req, file, next){
+     next(null, './public/UPLOAD');
+     },   
+      
+      //Then give the file a unique name
+      filename: function(req, file, next){
+          // console.log(file);
+          // const ext = file.mimetype.split('/')[1];
+          // next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+          next(null, file.originalname);
+        }
+      })   
+    };
+    
+    // https://lollyrock.com/articles/express4-file-upload/
+    // app.use(multer({ dest: './public/UPLOAD/'}));
+    // FORWARD THE FILE HANDLING PART TO MULTER
+    var file_handler = multer(multerConfig).single('pdf');
 
-//   // res.sendFile(__dirname + '/index.html');
-// });
+    app.post('/file-upload',file_handler, function(req,res){
+
+      console.log(req.body) 
+      console.log(req.file)
+      // res.send('Complete!');
+      
+   });
+
+
+
+
 
 
 app.use('/process_pdf', process_pdf);
--- routes/process_pdf.js
+++ routes/process_pdf.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const fs = require('fs');
+
+// FOR EXTRACTING IMAGE FROM PDF
 const pdf = require('pdf-poppler');
 
+// FOR UPLOADING PDF ...THIS CA BE REPLACED BY MORE ADVANCED MODULE
 var formidable = require('formidable');
+// FOR RUNNING TESSERACT
+const { exec } = require('child_process');
 
 var express = require('express');
 var router = express.Router();
@@ -14,8 +19,17 @@ var router = express.Router();
 router.post('/', function (req, res, next) {
 
 
-  var pdfname = req.query.pdf;
+    // var {language,start,end,pdf} = req.body;
+    // // var language = req.query.language,
+    // // start = req.query.start,
+    // // end = req.query.end;
+
+    // 
+ 
+    // res.send(a)
+
   // var uploaded_pdf = path.join(__dirname, '../public/UPLOAD', pdfname)
+
 
   
 
@@ -35,11 +49,15 @@ router.post('/', function (req, res, next) {
 
     // UPLOAD COMPLETE
     console.log('Uploaded ' + file.name);
-    //   res.send('Uploaded ' + file.name)
+    // res.send('Uploaded ' + file.name)
+    // res.render('process_pdf',{
+
+    //     language:
+    // })
 
 
     // CREATE IMAGES FROM UPLOADED PDF
-    create_image(file.path,file.name)
+    create_image(file.path,file.name,res)
 
     
 // 
@@ -50,44 +68,53 @@ router.post('/', function (req, res, next) {
 
 
 
-function create_image(filepath,filename){
+function create_image(filepath, filename, res) {
 
-    // var file = filepath;
- 
-// let file = 'Agniputra - Sunil Ganggopaddhya [amarboi.com].pdf'
-console.log("calling split")
- 
-let opts = {
-    format: 'jpeg',
-    out_dir: path.dirname(filepath),
-    // out_prefix: path.baseName(file, path.extname(file)),
-    out_prefix: filename,
-    page: null
-}
- 
-pdf.convert(filepath, opts)
-    .then(res => {
-        console.log('Successfully converted');
 
-        // REMOVE THE PDF
-        fs.unlinkSync(filepath)
-        // CREATE IMAGELIST
-        var UPLOAD = path.join(__dirname,'../public/UPLOAD')
-        // console.log(UPLOAD)
-        var data = fs.readdirSync(UPLOAD)
+    console.log("calling split")
 
-        data.forEach(element => {
+    let opts = {
+        format: 'jpeg',
+        out_dir: path.dirname(filepath),
+        // out_prefix: path.baseName(file, path.extname(file)),
+        out_prefix: filename,
+        page: null
+    }
 
-            fs.appendFileSync(path.join(UPLOAD,'../imagelist.txt'),path.join(UPLOAD,element)+'\n')    
-          
+    pdf.convert(filepath, opts)
+        .then(res => {
+            console.log('Successfully converted');
+
+            // REMOVE THE PDF
+            fs.unlinkSync(filepath)
+            // CREATE IMAGELIST
+            var UPLOAD = path.join(__dirname, '../public/UPLOAD')
+            // console.log(UPLOAD)
+            var data = fs.readdirSync(UPLOAD)
+
+            data.forEach(element => {
+
+                fs.appendFileSync(path.join(UPLOAD, '../imagelist.txt'), path.join(UPLOAD, element) + '\n')
+
+            })
+
+            // CALL TESSERACT
+            var imagelist = path.join(UPLOAD, '../imagelist.txt');
+            exec("start tesseract -l ben " + imagelist + " output", (err, stdout, stderr) => {
+                if (err) throw err;
+                // console.log(s)
+            })
+
+
+
+        })
+        .catch(error => {
+            console.error(error);
         })
 
-        // CALL TESSERACT
-        
-    })
-    .catch(error => {
-        console.error(error);
-    })
+    res.send("tesseract working")
+
+
 }
 
 
